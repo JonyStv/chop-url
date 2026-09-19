8ba52f301b3bc61a7b429ac13cf0157fe81f29f8
JonyStv <jonystever2006@gmail.com>
2026-09-19 18:02:30 +0200

    3

--- apps/api/middleware/cors.js
+++ apps/api/middleware/cors.js
@@ -6,7 +6,7 @@ export const corsMiddleware = cors({
     if (!origin) {
       return callback(null, true); // Permitir solicitudes sin origen (por ejemplo, desde Postman)
     }
-    if (env.defaultOrigins.includes(origin)) {
+    if (env.corsOrigins.includes(origin)) {
       return callback(null, true);
     }
     if (origin.endsWith(".vercel.app")) {
