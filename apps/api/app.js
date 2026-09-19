6053f04ba3f7e731dc36c6605ba82fffdf9fc54f
JonyStv <jonystever2006@gmail.com>
2026-09-19 11:41:16 +0200

    fix

--- apps/api/app.js
+++ apps/api/app.js
@@ -13,16 +13,6 @@ app.use(corsMiddleware);
 app.use(express.json());
 app.use(cookieParser());
 
-app.get("/api/test", (req, res) => {
-  res.json({
-    message: "Servidor Express en Vercel respondiendo correctamente",
-  });
-});
-
-app.get("/test", (req, res) => {
-  res.json({ message: "Servidor Express respondiendo en /test" });
-});
-
 app.use(["/users", "/api/users"], userRouter);
 app.use(["/auth", "/api/auth"], authRouter);
 app.use(["/links", "/api/links"], linksRouter);
